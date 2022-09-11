c6dee42d48276b9ed19efa4c7988dc47c41daf18
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-09-11 08:51:57 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router"
+import { Home } from "./pages/Home/Home"
+import './App.css'
+
 export function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:id' element={<div />} />
+      </Routes>
     </div>
   )
 }
