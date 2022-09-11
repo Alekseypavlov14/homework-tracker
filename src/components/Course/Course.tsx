1db3b74658043135ea52bfd23e62dfc5aed53570
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-09-11 11:35:41 +0300

    create util getColorById

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { colorDefaultSelector, colorsSelector } from '../../store/slices/colors/colorsSlice'
+import { getColorById } from '../../utils/getColorById/getColorById'
 import styles from './Course.module.css'
 
 interface CourseProps {
@@ -10,12 +11,14 @@ interface CourseProps {
 
 export const Course: FC<CourseProps> = ({ name, colorId }) => {
   const courseRef = useRef<HTMLDivElement>(null)
+
+  // colors
   const colors = useSelector(colorsSelector)
   const defaultColor = useSelector(colorDefaultSelector)
   
   useEffect(() => {
-    const color = colors.find(color => color.id === colorId) || defaultColor
-    courseRef.current?.style.setProperty('--color', color.value)
+    const color = getColorById(colors, defaultColor, colorId)
+    courseRef.current?.style.setProperty('--color', color)
   }, [colors, defaultColor, colorId])
 
   return (
--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -1,28 +1,40 @@
-import { FC, useState, useEffect } from 'react'
+import { FC, useState, useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
+import { colorDefaultSelector, colorsSelector } from '../../store/slices/colors/colorsSlice'
 import { coursesSelector } from '../../store/slices/courses/coursesSlice'
 import { Course } from '../../types/Course.interface'
+import { getColorById } from '../../utils/getColorById/getColorById'
 import styles from './CoursePage.module.css'
 
 interface CoursePageProps {}
 
 export const CoursePage: FC<CoursePageProps> = () => {
   const [course, setCourse] = useState<Course | null>(null)
-  const courses = useSelector(coursesSelector)
-  const navigate = useNavigate()
+  const coursePageRef = useRef<HTMLDivElement>(null)
+  
   const { id } = useParams()
+  const navigate = useNavigate()
+  
+  const courses = useSelector(coursesSelector)
+  const colors = useSelector(colorsSelector)
+  const defaultColor = useSelector(colorDefaultSelector)
 
   useEffect(() => {
     const course = courses.find(course => course.id === Number(id))
-
     if (course === undefined) return navigate('/')
+
     setCourse(course)
-  }, [courses, navigate, id])
+
+    const color = getColorById(colors, defaultColor, course.colorId)
+    coursePageRef.current?.style.setProperty('--color', color)
+  }, [colors, defaultColor, courses, navigate, id])
 
   return (
-    <div className={styles.CoursePage}>
-      {course?.name}
+    <div className={styles.CoursePage} ref={coursePageRef}>
+      <div className={styles.CourseHeader}>
+        {course?.name}
+      </div>
     </div>
   )
 }
