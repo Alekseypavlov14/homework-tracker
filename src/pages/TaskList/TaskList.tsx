854065e17e48440dbf03cb7e440e26a59d6f5ecc
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-09-14 10:47:06 +0300

    patch: task list shows only active tasks and tasks are sorted by course

--- src/pages/TaskList/TaskList.tsx
+++ src/pages/TaskList/TaskList.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react'
 import { useSelector } from 'react-redux'
 import { Header } from '../../components/Header/Header'
 import { Task } from '../../components/Task/Task'
+import { Task as TaskType } from './../../types/Task.interface'
 import { coursesSelector } from '../../store/slices/courses/coursesSlice'
-import { getCourseById } from '../../utils/getCourseById/getCourseById'
 import styles from './TaskList.module.css'
 
 interface TaskListProps {}
@@ -11,30 +11,34 @@ interface TaskListProps {}
 export const TaskList: FC<TaskListProps> = () => {
   const courses = useSelector(coursesSelector)
 
-  const tasks = courses
-    .map(course => course.tasks) 
-    .reduce((tasks, task) => tasks.concat(task), [])
+  const taskActiveFilterFunc = (task: TaskType) => !task.done
+
+  const coursesWithActiveTasks = courses.filter(course => {
+    return course.tasks.filter(taskActiveFilterFunc).length
+  })
 
   return (
     <div className={styles.TaskList}>
       <Header />
 
       <div className={styles.Tasks}>
-        {tasks.map(task => (
-          <div className={styles.TaskContainer}>
-            <div className={styles.TaskCourseName}>
-              {getCourseById(courses, task.courseId)?.name}
-            </div>
+        {coursesWithActiveTasks.map(course => (
+          <div className={styles.TaskSection}>
+            <div className={styles.TaskCourseName}>{course.name}</div>
 
-            <Task 
-              name={task.name}
-              deadline={task.deadline}
-              courseId={task.courseId}
-              isRequired={task.isRequired}
-              done={task.done}
-              id={task.id}
-              key={`${task.courseId}-${task.id}`}
-            />
+            {course.tasks.filter(taskActiveFilterFunc).map(task => (
+              <div className={styles.TaskContainer}>
+                <Task
+                  name={task.name}
+                  deadline={task.deadline}
+                  courseId={task.courseId}
+                  isRequired={task.isRequired}
+                  done={task.done}
+                  id={task.id}
+                  key={`${task.courseId}-${task.id}`}
+                />
+              </div>
+            ))}
           </div>
         ))}
       </div>
